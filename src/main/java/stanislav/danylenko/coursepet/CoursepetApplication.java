59cac769b00e866d0ded3e7f8b8b52bea035eff9
Stanislav Danylenko <stanislav.danylenko@gmail.com>
2019-03-15 00:01:34 +0200

    initial commit

---
+++ src/main/java/stanislav/danylenko/coursepet/CoursepetApplication.java
@@ -0,0 +1,13 @@
+package stanislav.danylenko.coursepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursepetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursepetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stanislav/danylenko/coursepet/CoursepetApplicationTests.java
@@ -0,0 +1,16 @@
+package stanislav.danylenko.coursepet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoursepetApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
