04235a524938d5c26f678f07d17d15306738c6f1
Stanislav Danylenko <stanislav.danylenko@gmail.com>
2019-04-02 02:28:32 +0300

    Added statistic controller

---
+++ src/main/java/stanislav/danylenko/coursepet/web/controller/StatisticController.java
@@ -0,0 +1,24 @@
+package stanislav.danylenko.coursepet.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import stanislav.danylenko.coursepet.service.impl.bl.StatisticService;
+import stanislav.danylenko.coursepet.web.model.statistic.CountByBreedInCountryStatisticDto;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/statistic")
+public class StatisticController {
+
+    @Autowired
+    private StatisticService statisticService;
+
+    @GetMapping("/test/{countryId}")
+    public @ResponseBody
+    ResponseEntity<List<CountByBreedInCountryStatisticDto>> getStat(@PathVariable("countryId") Long countryId) {
+        return ResponseEntity.ok(statisticService.getCountByBreedInCountryStatistic(countryId));
+    }
+
+}
